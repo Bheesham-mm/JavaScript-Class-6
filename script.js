1f7fdc7e684f1f56b7f6fc43a5390474f6216ff5
maheshdkushlani <maheshdkushlani@gmail.com>
2024-08-15 07:15:45 +0500

    add files

---
+++ script.js
@@ -0,0 +1,15 @@
+let a = Number(prompt("Enter a number"));
+
+if (a > 0) {
+    alert("Number is positive.");
+    console.log("Number is positive.");
+}
+if (a < 1) {
+    alert("Number is negative.");
+    console.log("Number is negative.");
+}
+if (a === 0) {
+    alert("Number is zero.");
+    console.log("Number is zero.");
+}
+console.log(a);
